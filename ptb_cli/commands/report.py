e97fafa71be4fe7001a308d1d12b9969bb5fc25e
Vitto Torres <vittotq@gmail.com>
2025-04-15 23:45:49 -0500

    Habilitacion de -help en report

--- ptb_cli/commands/report.py
+++ ptb_cli/commands/report.py
@@ -1,19 +1,39 @@
 import typer
-from ptb_cli.utils.io import read_csv
-from ptb_cli.utils.dataAnalisis import (calcularBalanceFinal
-                                        ,calcularTransaccionMontoMayor
-                                        ,contarTransaccionesPorTipo
-                                        )
+from pathlib import Path
 from typing import List, Dict
+from ptb_cli.utils.io import read_csv
+from ptb_cli.utils.dataAnalisis import (
+    calcularBalanceFinal,
+    calcularTransaccionMontoMayor,
+    contarTransaccionesPorTipo,
+)
 
 app = typer.Typer()
 
 globalCsvData: List[Dict[str, str]] = []
 
-def set_data(data):
-    global globalCsvData
-    globalCsvData = data
+@app.callback()
+def fetch_data(
+    ctx: typer.Context,
+    file: str = typer.Option(None, "--file", "-f", help="Ruta del CSV")
+):
+    if ctx.resilient_parsing:
+        return
 
+    if file is None:
+        typer.secho("❌ Debes especificar un archivo con --file", fg=typer.colors.RED)
+        raise typer.Exit(code=1)
+
+    try:
+        path = Path(file)
+        if not path.exists():
+            raise FileNotFoundError(f"❌ El archivo '{file}' no existe.")
+
+        global globalCsvData
+        globalCsvData = read_csv(file)
+    except (FileNotFoundError, ValueError) as e:
+        typer.secho(str(e), fg=typer.colors.RED)
+        raise typer.Exit(code=1)
 
 
 @app.command(name="full-report"
@@ -28,7 +48,21 @@ def full_report():
     balanceFinal=calcularBalanceFinal(globalCsvData)
     transaccionDeMontoMayor=calcularTransaccionMontoMayor(globalCsvData)
     cantidadDeTransacciones=contarTransaccionesPorTipo(globalCsvData)
-    print(balanceFinal)
-    print(transaccionDeMontoMayor)
-    print(cantidadDeTransacciones)
+    print("Reporte de transacciones")
+    print("---------------------------------------------")
+
+    ## Display de balance de transacciones total
+    print("Balance Final : ", round(float(balanceFinal),2))
+
+    ## Display de transaccion de mayor monto
+    print("Transacción de Mayor Monto: ID "
+          ,transaccionDeMontoMayor['id']
+          ," - "
+          ,round(float(transaccionDeMontoMayor['monto']),2))
     
+    ## Display de conteo de transacciones por tipo
+    print("Conteo de Transacciones: Crédito: "
+          ,cantidadDeTransacciones['crédito']
+          ," Débito: "
+          ,cantidadDeTransacciones['débito']
+          )
